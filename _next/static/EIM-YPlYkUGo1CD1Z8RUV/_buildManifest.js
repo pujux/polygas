34022dd551ce0deec76daad48fbf2fe8f67271e7
Julian Pufler <julian.pufler@lean-coders.at>
2021-09-27 14:54:23 +0200

    Deploy Next.js to gh-pages

---
+++ _next/static/EIM-YPlYkUGo1CD1Z8RUV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F36bcf0ca.6e0bed45717f2f1e2d26.js","static\u002Fchunks\u002Fpages\u002Findex-c8ac44196c2bc67f1aef.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-67461efa3dd05e050c04.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/icVndXytxmY4qrjVglBAx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F36bcf0ca.6e0bed45717f2f1e2d26.js","static\u002Fchunks\u002Fpages\u002Findex-9a32e7b7e75cf4ee5c79.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-67461efa3dd05e050c04.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
